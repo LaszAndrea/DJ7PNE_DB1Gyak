491fc0dc95710ebf408ba76bb60a95d50ca0e211
Andrea@DESKTOP-2UKDU76.(none) <laszlo.andika@citromail.hu>
2021-10-05 01:08:46 +0200

    03_08

---
+++ DJ7PNE_0929/DJ7PNE_03_08/src/dj7pne/DJ7PNE_03_08_futtat.java
@@ -0,0 +1,21 @@
+package dj7pne;
+
+
+//import  dj7pne.DJ7PNE_03_08;
+
+
+public class DJ7PNE_03_08_futtat {
+
+	public static void main(String[] args) {
+
+		/*DJ7PNE_03_08 adatok[] = DJ7PNE_03_08.beker();
+		
+		for (int i = 0; i < adatok.length; i++) {
+			System.out.println(adatok[i]);
+		}*/
+		
+		
+		
+	}
+	
+}
